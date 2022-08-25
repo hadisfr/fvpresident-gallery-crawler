a48a666f65b9623028b2344307e2e63e65ab01c1
H@di <hadi@hadisafari.ir>
2022-08-26 01:45:07 +0430

    use tqdm

--- crawler.py
+++ crawler.py
@@ -7,24 +7,22 @@ import logging
 from os import path
 from pathlib import Path
 from typing import Any, Dict, Iterable
-from sys import argv, stderr
-from pprint import pprint
 
 import requests
+from tqdm import trange
 
 logging.captureWarnings(True)
 
-# details_patter = re.compile(r"(?s)<div class=\"ug-textpanel-description\".*?>(.*?)<span.*?>(.*?)</span><span class=\"wrapper\"></span></div>")
 details_patter = re.compile(r"(?s)<img\s+alt=\"(.*?)\"\s+src=\"(.*?)\"\s+data-image=\"(.*?)\"\s+data-description='(.*?)'\s+/>")
+description_regex = re.compile(r"(.*)<span class=\"date_author_news\">(.*)</span><span class=\"wrapper\"></span>")
 
 
 def parse_page(page_id: int) -> Iterable[Dict[str, Any]]:
     def parse_description(image: Dict[str, Any]) -> Dict[str, Any]:
-        description_regex = re.compile(r"(.*)<span class=\"date_author_news\">(.*)</span><span class=\"wrapper\"></span>")
         description = image["data-description"]
         parsed = description_regex.findall(description)
         if len(parsed) != 1 or len(parsed[0]) != 2:
-            print("invalid data-description in page %s:\t%s" % (image["page_id"], image["data-description"]), file=stderr)
+            print("invalid data-description in page %s:\t%s" % (image["page_id"], image["data-description"]))
             image["date_author_news"] = ""
             return image
         description, date_author = parsed[0]
@@ -34,7 +32,7 @@ def parse_page(page_id: int) -> Iterable[Dict[str, Any]]:
 
     def parse_image(image: Dict[str, Any]) -> Dict[str, Any]:
         if image["alt"] != "" or image["src"] != image["data-image"] or not image["date_author_news"].startswith("تاریخ انتشار: "):
-            print("invalid fields:\n%s" % json.dumps(image), file=stderr)
+            print("invalid fields:\n%s" % json.dumps(image))
             return image
         return {
             "Page ID": image["page_id"],
@@ -50,11 +48,11 @@ def parse_page(page_id: int) -> Iterable[Dict[str, Any]]:
     url = "https://fvpresident.ir/fa/gallery/%d" % page_id
     page = requests.get(url, verify=False)
     if page.status_code != 200:
-        print("could not get %s: %s" % (page_id, page.status_code), file=stderr)
+        print("could not get %s: %s" % (page_id, page.status_code))
         return []
     images = details_patter.findall(page.text)
     if not len(images):
-        print("no image found in %s" % page_id, file=stderr)
+        print("no image found in %s" % page_id)
     images = map(lambda img: {
         "page_id": page_id,
         "alt": img[0].strip(),
@@ -71,7 +69,7 @@ def parse_page(page_id: int) -> Iterable[Dict[str, Any]]:
 def download_img(img_url: str, page_id: str) -> None:
     response = requests.get(img_url, stream=True, verify=False)
     if response.status_code != 200:
-        print("could not get image %s: %s" % (img_url, response.status_code), file=stderr)
+        print("could not get image %s: %s" % (img_url, response.status_code))
         return
     dir_addr = path.join("pics", str(page_id))
     file_addr = path.join(dir_addr, img_url.split("/")[-1])
@@ -84,7 +82,7 @@ def download_img(img_url: str, page_id: str) -> None:
 def main():
     LAST_PAGE_ID = 8356
     with open("metadata.jsonl", "w") as f:
-        for page_id in range(1, LAST_PAGE_ID + 1):
+        for page_id in trange(1, LAST_PAGE_ID + 1):
             for image in parse_page(page_id):
                 print(json.dumps(image, ensure_ascii=False), file=f)
 
